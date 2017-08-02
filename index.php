3074f2e0b4df31408ca87094876a1656e5e74df4
Seynabou08 <ndiaye_seynabou@hotmail.ca>
2017-08-02 14:06:56 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 <html>
 <head>
     <title>EDEL</title>
+
     <?php include './template/includes.html' ?>
 </head>
 
@@ -28,9 +29,9 @@
     <div id="splashScreen">
         <img src="https://storage.moqups.com/repo/iNXjFx1a/images/iPCgz6naJD.png" id="imgSplashScreen">
       
-        <h2 id="slogan"> Build for Refugees and Asylum Seekers </h2>
+        <h2 id="slogan"> Built for Refugees and Asylum Seekers </h2>
 
-        <h4 id="subSlogan"> Get answers fast and confidentially </h4>
+        <h4 id="subSlogan"> Get the answers you need in a simple and efficient way </h4>
         <div class="downloadApp">
             <img class="downloadAppImages" src="https://storage.moqups.com/repo/iNXjFx1a/images/jsgz4kcW2q.png">
             <img class= "downloadAppImages" src="https://storage.moqups.com/repo/iNXjFx1a/images/A6v9HOv8On.png">
@@ -49,138 +50,201 @@
     
     <!-- add script -->
     <script src="js/register-modal.js"> </script>
-    <!-- Page content -->
-    <div class="w3-content" class="newStyle">
-        <!-- The Band Section -->
-        <div class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="band">
-            <h2 class="w3-wide">EDEL</h2>
-            <p class="w3-opacity"><i>We love refugees</i></p>
-            <p class="w3-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse molestie arcu volutpat est interdum, at egestas orci facilisis. Praesent vitae est vel nibh finibus consequat eget non erat. Cras vulputate tincidunt mauris nec facilisis. Integer mollis maximus tristique. Proin ac libero a orci ornare suscipit. Sed tempor nunc eu nisl venenatis, ac feugiat sem maximus. Maecenas enim lorem, pulvinar elementum dolor et, feugiat faucibus massa. Phasellus eu gravida quam, non euismod diam.</p>
-            <div class="w3-row w3-padding-32">
-                <div class="w3-third">
-                    <p>Name</p>
-                    <img src="images/here.png" class="w3-round w3-margin-bottom" alt="Random Name" style="width:60%">
-                </div>
-                <div class="w3-third">
-                    <p>Name</p>
-                    <img src="images/here.png" class="w3-round w3-margin-bottom" alt="Random Name" style="width:60%">
-                </div>
-                <div class="w3-third">
-                    <p>Name</p>
-                    <img src="images/here.png" class="w3-round" alt="Random Name" style="width:60%">
-                </div>
-            </div>
-        </div>
-        <!-- The Tour Section -->
-        <div class="w3-black" id="tour">
-            <div class="w3-container w3-content w3-padding-64" style="max-width:800px; width:100%;">
-                <h2 class="w3-wide w3-center">SOMETHING</h2>
-                <p class="w3-opacity w3-center"><i>Remember to book your tickets!</i></p>
-                <br>
-                <ul class="w3-ul w3-border w3-white w3-text-grey">
-                    <li class="w3-padding">September <span class="w3-tag w3-red w3-margin-left">Sold out</span></li>
-                    <li class="w3-padding">October <span class="w3-tag w3-red w3-margin-left">Sold out</span></li>
-                    <li class="w3-padding">November <span class="w3-badge w3-right w3-margin-right">3</span></li>
-                </ul>
-                <div class="w3-row-padding w3-padding-32" style="margin:0 -16px">
-                    <div class="w3-third w3-margin-bottom">
-                        <div class="w3-container w3-white">
-                            <p><b>New York</b></p>
-                            <p class="w3-opacity">Fri 27 Nov 2016</p>
-                            <p>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
-                            <button class="w3-button w3-black w3-margin-bottom" onclick="document.getElementById('ticketModal').style.display='block'">Buy Tickets</button>
-                        </div>
-                    </div>
-                    <div class="w3-third w3-margin-bottom">
-                        <div class="w3-container w3-white">
-                            <p><b>Paris</b></p>
-                            <p class="w3-opacity">Sat 28 Nov 2016</p>
-                            <p>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
-                            <button class="w3-button w3-black w3-margin-bottom" onclick="document.getElementById('ticketModal').style.display='block'">Buy Tickets</button>
-                        </div>
-                    </div>
-                    <div class="w3-third w3-margin-bottom">
-                        <div class="w3-container w3-white">
-                            <p><b>San Francisco</b></p>
-                            <p class="w3-opacity">Sun 29 Nov 2016</p>
-                            <p>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
-                            <button class="w3-button w3-black w3-margin-bottom" onclick="document.getElementById('ticketModal').style.display='block'">Buy Tickets</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+   
 
-        <!-- Ticket Modal -->
-        <div id="ticketModal" class="w3-modal">
-            <div class="w3-modal-content w3-animate-top w3-card-4">
-                <header class="w3-container w3-teal w3-center w3-padding-32">
-                    <span onclick="document.getElementById('ticketModal').style.display='none'" class="w3-button w3-teal w3-xlarge w3-display-topright">×</span>
-                    <h2 class="w3-wide"><i class="fa fa-suitcase w3-margin-right"></i>Tickets</h2>
-                </header>
-                <div class="w3-container">
-                    <p>
-                        <label><i class="fa fa-shopping-cart"></i> Tickets, $15 per person</label>
-                    </p>
-                    <input class="w3-input w3-border" type="text" placeholder="How many?">
-                    <p>
-                        <label><i class="fa fa-user"></i> Send To</label>
-                    </p>
-                    <input class="w3-input w3-border" type="text" placeholder="Enter email">
-                    <button class="w3-button w3-block w3-teal w3-padding-16 w3-section w3-right">PAY <i class="fa fa-check"></i></button>
-                    <button class="w3-button w3-red w3-section" onclick="document.getElementById('ticketModal').style.display='none'">Close <i class="fa fa-remove"></i></button>
-                    <p class="w3-right">Need <a href="#" class="w3-text-blue">help?</a></p>
+
+
+
+     <!-- Page content -->
+        <div class="w3-content" class="newStyle">
+            <!-- The Band Section -->
+            <div class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="band">
+                <h2 class="w3-wide">EDEL</h2>
+                <p class="w3-opacity"><i>Find what you need when you need it</i></p>
+                <p class="w3-justify"> We have gather here the information and tools you need for a smooth transition into your new home.</p>
+                <div class="w3-row w3-padding-32">
+
+                    <div class="row">
+                        <div class="col-sm-4" style="float:left">
+                            <!--p style="text-align: center">Housing</p-->
+                            <img src="images/circle.png" alt="Random Name" style="position: absolute; margin-left:4em">
+                            <img src="images/home.png" alt="Random Name"   style="position: absolute; width:60px; padding:8x; margin-left:4em">
+                        </div>
+
+                        <div class="col-sm-4" style="">
+                            <!--p style="text-align: center">Health Care</p-->
+                            <img src="images/circle.png" alt="Random Name" style="position: absolute; margin-left:4em">
+                            <img src="images/kit.png" alt="Random Name"    style="position: absolute; width:60px; padding:8px; margin-left:4em">
+                        </div>
+
+                        <div class="col-sm-4" style="float:right">
+                            <!--p style="text-align: center">Education</p-->
+                            <img src="images/circle.png" alt="Random Name" style="position: absolute; margin-left:4em">
+                            <img src="images/books.png" alt="Random Name"  style="position: absolute; width:60px; padding:8px; margin-left:4em">
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <!-- The Contact Section -->
-        <div class="w3-container w3-content w3-padding-64" style="max-width:800px" id="contact">
-            <h2 class="w3-wide w3-center">CONTACT</h2>
-            <p class="w3-opacity w3-center"><i>Fan? Drop a note!</i></p>
-            <div class="w3-row w3-padding-32">
-                <div class="w3-col m6 w3-large w3-margin-bottom">
-                    <i class="fa fa-map-marker" style="width:30px"></i> Chicago, US
-                    <br>
-                    <i class="fa fa-phone" style="width:30px"></i> Phone: +00 151515
-                    <br>
-                    <i class="fa fa-envelope" style="width:30px"> </i> Email: mail@mail.com
-                    <br>
+
+            <div class="row" style="width: 100%; height: 27em">
+                <img src="images/background.png" style="position: absolute; left:0; width: 100%; height: 27em">
+
+                <div class="row">
+
+                    <div class="col-sm-5" style="float:left">
+                        <img src="images/Desktop.png" alt="desktop" style=" width: 65%; margin-top:9em">
+                        <img src="images/phone.png" alt="mobile" style="width: 45%; margin-top:12em; margin-left:-5em">
+                    </div>
+
+                    <div class="col-sm-5" style="float:right">
+                        <h2 style="color: white; text-align: center; margin: auto 0; padding-top:5em; width: 100%; font-weight: bold; opacity: 0.99;"> Perfect for plannig ahead or using on the go</h2>
+
+                        <h4 id="subSlogan"> Whether you are at home or on the street, access your account from the platform of your liking.
+                            Our chatbot and  forums can be accessed by web or by using our mobile application</h4>
+
+                        </div>
+                    </div>
                 </div>
-                <div class="w3-col m6">
-                    <form action="/action_page.php" target="_blank">
-                        <div class="w3-row-padding" style="margin:0 -16px 8px -16px">
-                            <div class="w3-half">
-                                <input class="w3-input w3-border" type="text" placeholder="Name" required name="Name">
+
+
+                <div class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="band">
+
+
+                    <div class="col-sm-5" style="float:left">
+                       <h2 style="color: black; text-align: center; margin: auto 0; width: 100%; font-weight: bold; opacity: 0.99;">
+                        Connect with a community of peers and experts</h2>
+
+                        <h4 id="subSlogan" style="color:black"> With this tool, you can get directed to the proper organisations that seek to help you. 
+                            To find out why NGO's recommend our service <a href="pages/aboutUs.php"> click here!</a> </h4>
+
+
+
+                        </div>
+
+                        <div class="col-sm-5" style="float:right">
+
+                        </div>
+
+
+
+
+
+
+                    </div>
+
+
+                    <div class="row" style="width: 100%; height: 40em">
+                        <img src="images/background2.png" style="position: absolute; left:0; width: 100%; height: 40em; z-index:-1">
+                        <div class="row">
+
+                            <h2 style="color:white; margin-top:3em; margin-left:2em">CONTACT</h2>
+                            <p class="w3-left" style="margin-left:4.4em; color: white;  "><strong>If you have any question or comments you would like to share with us, feel free to reach out to us!</strong></p>
+                            <div class="w3-row w3-padding-32">
+
+                                <div class="w3-col m6"  style="float:left">
+                                    <form action="/action_page.php" target="_blank">
+                                        <div class="w3-row-padding" style="margin:0 -16px 8px -16px">
+                                            <div class="w3-half">
+                                                <input class="w3-input w3-border" type="text" placeholder="Name" required name="Name">
+                                            </div>
+                                            <div class="w3-half">
+                                                <input class="w3-input w3-border" type="text" placeholder="Email" required name="Email">
+                                            </div>
+                                        </div>
+                                        <input class="w3-input w3-border" type="text" placeholder="Message" required name="Message" style="height:15em">
+                                        <button class="w3-button w3-black w3-section w3-right" type="submit">SEND</button>
+                                    </form>
+
+                                    <div> 
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-4" style="float:right; margin:3em">
+
+                                    <h3 style="color: white;  margin: auto 0; font-weight: bold"> <i class="fa fa-map-marker" style="width:30px"></i> Montreal, Qc </h4>
+                                        <br>
+
+
+                                        <h3 style="color: white;  margin: auto 0; font-weight: bold"> <i class="fa fa-phone" style="width:30px"></i> Phone: +00 1 (514)555-5555 </h4>
+                                            <br>
+
+
+                                            <h3 style="color: white; margin: auto 0; font-weight: bold"><i class="fa fa-envelope" style="width:30px"> </i>Email: mail@mail.com </h4>
+                                                <br>
+
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- End Page Content -->
+                                </div>
+
+                                <!-- Subscribe Section -->
+                                <div class="w3-container w3-content w3-padding-64" style="max-width:800px" id="contact">
+                                    <h2 class="w3-wide w3-center">Never Miss A Thing </h2>
+                                    <p class="w3-opacity w3-center">For regular updates about our products, subscribe to our newsletter</p>
+
+                                    <button class="w3-button w3-black w3-section w3-right" type="submit" style="float:left">SUBSCRIBE</button>
+
+                                </div>
+                                <!-- End Page Content -->
                             </div>
-                            <div class="w3-half">
-                                <input class="w3-input w3-border" type="text" placeholder="Email" required name="Email">
-                            </div>
-                        </div>
-                        <input class="w3-input w3-border" type="text" placeholder="Message" required name="Message">
-                        <button class="w3-button w3-black w3-section w3-right" type="submit">SEND</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-        <!-- End Page Content -->
-    </div>
 
-    <!-- Footer -->
-    <footer class="w3-container w3-padding-64 w3-center w3-opacity w3-light-grey w3-xlarge">
-        <i class="fa fa-facebook-official w3-hover-opacity"></i>
-        <i class="fa fa-instagram w3-hover-opacity"></i>
-        <i class="fa fa-snapchat w3-hover-opacity"></i>
-        <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-        <i class="fa fa-twitter w3-hover-opacity"></i>
-        <i class="fa fa-linkedin w3-hover-opacity"></i>
-        <p class="w3-medium">Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
-    </footer>
+                            <!-- Footer -->
+                            <footer class="w3-container w3-padding-64 w3-center w3-opacity w3-light-grey w3-xlarge">
+                                <i class="fa fa-facebook-official w3-hover-opacity"></i>
+                                <i class="fa fa-instagram w3-hover-opacity"></i>
+                                <i class="fa fa-snapchat w3-hover-opacity"></i>
+                                <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+                                <i class="fa fa-twitter w3-hover-opacity"></i>
+                                <i class="fa fa-linkedin w3-hover-opacity"></i>
+                                <p class="w3-medium">Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+                            </footer>
+                            <script>
+    // Automatic Slideshow - change image every 4 seconds
+    var myIndex = 0;
+    carousel();
+
+    function carousel() {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        myIndex++;
+        if (myIndex > x.length) {
+            myIndex = 1
+        }
+        x[myIndex - 1].style.display = "block";
+        setTimeout(carousel, 4000);
+    }
+
+    // Used to toggle the menu on small screens when clicking on the menu button
+    function myFunction() {
+        var x = document.getElementById("navDemo");
+        if (x.className.indexOf("w3-show") == -1) {
+            x.className += " w3-show";
+        } else {
+            x.className = x.className.replace(" w3-show", "");
+        }
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    var modal = document.getElementById('ticketModal');
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    </script>
 </body>
 
 <script>
     //registering handlers
     registerBody();
     window.addEventListener("scroll", update);
-</script>
-</html>
+    </script>
+    </html>
+
